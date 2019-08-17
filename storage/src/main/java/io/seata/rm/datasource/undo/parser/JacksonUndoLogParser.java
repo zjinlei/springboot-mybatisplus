f96b371acac58b8606293280338385f0b1a25af6
zjinlei <75718677@qq.com>
2019-08-17 23:17:47 +0800

    copy 0.8.0 rm datasource to be fix bug

---
+++ storage/src/main/java/io/seata/rm/datasource/undo/parser/JacksonUndoLogParser.java
@@ -0,0 +1,160 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package io.seata.rm.datasource.undo.parser;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.core.type.WritableTypeId;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.deser.std.JsonNodeDeserializer;
+import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.ser.std.ArraySerializerBase;
+import io.seata.common.Constants;
+import io.seata.common.loader.LoadLevel;
+import io.seata.rm.datasource.undo.BranchUndoLog;
+import io.seata.rm.datasource.undo.UndoLogParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+
+/**
+ * The type Json based undo log parser.
+ *
+ * @author jsbxyyx
+ */
+@LoadLevel(name = io.seata.rm.datasource.undo.parser.JacksonUndoLogParser.NAME)
+public class JacksonUndoLogParser implements UndoLogParser {
+
+    public static final String NAME = "jackson";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(io.seata.rm.datasource.undo.parser.JacksonUndoLogParser.class);
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    private static final SimpleModule MODULE = new SimpleModule();
+
+    /**
+     * customize serializer for java.sql.Timestamp
+     */
+    private static final JsonSerializer TIMESTAMP_SERIALIZER = new TimestampSerializer();
+
+    /**
+     * customize deserializer for java.sql.Timestamp
+     */
+    private static final JsonDeserializer TIMESTAMP_DESERIALIZER = new TimestampDeserializer();
+
+    static {
+        MODULE.addSerializer(Timestamp.class, TIMESTAMP_SERIALIZER);
+        MODULE.addDeserializer(Timestamp.class, TIMESTAMP_DESERIALIZER);
+        MAPPER.registerModule(MODULE);
+        MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        MAPPER.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public byte[] getDefaultContent() {
+        return "{}".getBytes(Constants.DEFAULT_CHARSET);
+    }
+
+    @Override
+    public byte[] encode(BranchUndoLog branchUndoLog) {
+        try {
+            byte[] bytes = MAPPER.writeValueAsBytes(branchUndoLog);
+            return bytes;
+        } catch (JsonProcessingException e) {
+            LOGGER.error("json encode exception, {}", e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public BranchUndoLog decode(byte[] bytes) {
+        try {
+            BranchUndoLog branchUndoLog = MAPPER.readValue(bytes, BranchUndoLog.class);
+            return branchUndoLog;
+        } catch (IOException e) {
+            LOGGER.error("json decode exception, {}", e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * if necessary
+     * extend {@link ArraySerializerBase}
+     */
+    private static class TimestampSerializer extends JsonSerializer<Timestamp> {
+
+        @Override
+        public void serializeWithType(Timestamp timestamp, JsonGenerator gen, SerializerProvider serializers, TypeSerializer typeSerializer) throws IOException {
+            WritableTypeId typeId = typeSerializer.writeTypePrefix(gen, typeSerializer.typeId(timestamp, JsonToken.START_ARRAY));
+            serialize(timestamp, gen, serializers);
+            gen.writeTypeSuffix(typeId);
+        }
+
+        @Override
+        public void serialize(Timestamp timestamp, JsonGenerator gen, SerializerProvider serializers) {
+            try {
+                gen.writeNumber(timestamp.getTime());
+                gen.writeNumber(timestamp.getNanos());
+            } catch (IOException e) {
+                LOGGER.error("serialize java.sql.Timestamp error : {}", e.getMessage(), e);
+            }
+
+        }
+    }
+
+    /**
+     * if necessary
+     * extend {@link JsonNodeDeserializer}
+     */
+    private static class TimestampDeserializer extends JsonDeserializer<Timestamp> {
+
+        @Override
+        public Timestamp deserialize(JsonParser p, DeserializationContext ctxt) {
+            if(p.isExpectedStartArrayToken()){
+                ArrayNode arrayNode;
+                try {
+                    arrayNode = p.getCodec().readTree(p);
+                    Timestamp timestamp = new Timestamp(arrayNode.get(0).asLong());
+                    timestamp.setNanos(arrayNode.get(1).asInt());
+                    return timestamp;
+                } catch (IOException e) {
+                    LOGGER.error("deserialize java.sql.Timestamp error : {}", e.getMessage(), e);
+                }
+            }
+            LOGGER.error("deserialize java.sql.Timestamp type error.");
+            return null;
+        }
+    }
+
+}
