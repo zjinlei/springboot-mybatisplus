9fa9a3756ecd3fce93b5b7aabbdb85b705a51041
zjinlei <75718677@qq.com>
2019-12-08 16:19:54 +0800

    upgrade

--- storage/src/main/java/com/helios/seata/storage/config/DataBaseConfiguration.java
+++ storage/src/main/java/com/helios/seata/storage/config/DataBaseConfiguration.java
@@ -1,16 +1,14 @@
 package com.helios.seata.storage.config;
 
+import javax.sql.DataSource;
+import java.io.IOException;
+
 import com.alibaba.druid.pool.DruidDataSource;
 import com.baomidou.mybatisplus.spring.MybatisSqlSessionFactoryBean;
-import io.seata.rm.datasource.DataSourceProxy;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.ResourcePatternResolver;
-
-import javax.sql.DataSource;
-import java.io.IOException;
 
 @Configuration
 public class DataBaseConfiguration {
@@ -22,15 +20,8 @@ public class DataBaseConfiguration {
         return druidDataSource;
     }
 
-    @Primary
-    @Bean("dataSource")
-    public DataSourceProxy dataSource(DataSource druidDataSource) {
-        return new DataSourceProxy(druidDataSource);
-    }
-
-
     @Bean
-    public MybatisSqlSessionFactoryBean mybatisSqlSessionFactoryBean(DataSourceProxy druidDataSource, ResourcePatternResolver resourcePatternResolver) throws IOException {
+    public MybatisSqlSessionFactoryBean mybatisSqlSessionFactoryBean(DataSource druidDataSource, ResourcePatternResolver resourcePatternResolver) throws IOException {
         MybatisSqlSessionFactoryBean mybatisSqlSessionFactoryBean = new MybatisSqlSessionFactoryBean();
         mybatisSqlSessionFactoryBean.setDataSource(druidDataSource);
         mybatisSqlSessionFactoryBean.setMapperLocations(resourcePatternResolver.getResources("classpath:mapper/*.xml"));
