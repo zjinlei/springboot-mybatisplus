5ed8af98ff856928e49903ddbe72169f86fdcd9a
zhuangjinlei <jinlei.zhuang@huilianyi.com>
2020-01-23 11:06:30 +0800

    update starter

--- business/src/main/java/com/helios/seata/business/BusinessApplication.java
+++ business/src/main/java/com/helios/seata/business/BusinessApplication.java
@@ -2,8 +2,9 @@ package com.helios.seata.business;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages={"com.helios.seata"})
+@SpringBootApplication(scanBasePackages={"com.helios.seata"}, exclude = DataSourceAutoConfiguration.class)
 public class BusinessApplication {
 
 	public static void main(String[] args) {
