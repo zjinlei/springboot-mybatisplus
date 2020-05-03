c1ef913f826efe4392742b0ca12d4868f5223d0e
zjinlei <75718677@qq.com>
2020-05-03 10:29:40 +0800

    add sql

--- storage/src/main/java/com/helios/seata/storage/persistence/StorageMapper.java
+++ storage/src/main/java/com/helios/seata/storage/persistence/StorageMapper.java
@@ -12,7 +12,12 @@ public interface StorageMapper extends BaseMapper<Storage> {
     @Select("select id, commodity_code commodityCode, count from storage_tbl where commodity_code =#{commodityCode}")
     Storage findByCommodityCode(@Param("commodityCode") String commodityCode);
 
+    @Delete("delete tb from storage_tbl tb where tb.commodity_code =#{commodityCode}")
+    void deltByCommodityCode(@Param("commodityCode") String commodityCode);
+
     void insertBatch(List<Storage> records);
 
+    @Update("update storage_tbl tb set count=1 where tb.commodity_code =#{commodityCode}")
+    void updateByCommodityCode(@Param("commodityCode") String commodityCode);
     int updateBatch(@Param("list") List<Long> ids,@Param("commodityCode") String commodityCode);
 }
